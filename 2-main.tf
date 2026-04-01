cbe1d97e4e3cf8e57313c7b70b4786bb3fb526ae
Vince M <231880360+eiht-bit@users.noreply.github.com>
2026-03-31 22:27:41 -0700

    updated

--- 2-main.tf
+++ 2-main.tf
@@ -7,6 +7,34 @@ resource "aws_s3_bucket" "fried" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "fried" {
+  bucket = aws_s3_bucket.fried.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "fried" {
+  bucket = aws_s3_bucket.fried.id
+
+  depends_on = [aws_s3_bucket_public_access_block.fried]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.fried.arn}/*"
+      }
+    ]
+  })
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.fried.id
 }
